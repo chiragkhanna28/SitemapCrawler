e3ce5fedc6cb30ba61eac5a4dafa74dbd0aaa720
Chirag Khanna <ckhanna@horizontal.com>
2023-11-18 23:19:28 +0530

    made changes for sitemap crawler

--- src/Foundation/SitemapCrawler/LinkManagers/CustomLinkProvider.cs
+++ src/Foundation/SitemapCrawler/LinkManagers/CustomLinkProvider.cs
@@ -5,19 +5,24 @@ using Sitecore.Links;
 using Sitecore.Links.UrlBuilders;
 using System;
 using Microsoft.Extensions.DependencyInjection;
+using Sitecore.XA.Foundation.Multisite;
+using Sitecore.Web;
 
 namespace SitemapCrawler.LinkManagers
 {
     public class CustomLinkProvider : LinkProvider
     {
+        private ISiteInfoResolver SiteInfoResolver { get; set; }
         public CustomLinkProvider() : base(ServiceLocator.ServiceProvider.GetService<BaseFactory>())
         {
+            SiteInfoResolver = ServiceLocator.ServiceProvider.GetService<ISiteInfoResolver>();
         }
         public override string GetItemUrl(Item item, ItemUrlBuilderOptions options)
         {
             if (item != null && item.TemplateName.Equals(Constants.ArticlePage, StringComparison.OrdinalIgnoreCase))
             {
-                return string.Format("/{0}/{1}", Constants.ArticlesUrl, item.Name.Replace(" ", "-"));
+                SiteInfo site = this.SiteInfoResolver.GetSiteInfo(item);
+                return string.Format("https://{0}/{1}/{2}", site.HostName, Constants.ArticlesUrl, item.Name.Replace(" ", "-"));
             }
             return base.GetItemUrl(item, options);
         }
